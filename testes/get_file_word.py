9d4ed17d92961de10917eaaff979bc420020e500
CNI <walleu@gmail.com>
2020-09-03 14:06:12 -0300

    melhoria metodo get_file_word

--- testes/get_file_word.py
+++ testes/get_file_word.py
@@ -20,35 +20,45 @@ from os.path import isfile, join
 #fileDir = r"/Users/wpessoa/repositorios/Fluxo_Editorial/words/"
 #fileExt = r".docx"
 
-def get_info(fileDir, fileExt,i):
+def get_info(fileDir,files_array):
     while True:
         try:
-            print(os.path.exists(fileDir)) 
-            files_array = [_ for _ in os.listdir(fileDir) if _.endswith(fileExt)]
-            indice = i
+            #print(os.path.exists(fileDir, fileDir)) 
+            #files_array = [_ for _ in os.listdir(fileDir) if _.endswith(fileExt)]
+            indice = 0
             image_array=[]
             table_chunks = []
+            table_return = []
+            
+            for file in files_array:
+              
+                # abrir conecção com o arquivo Word 
+                doc = docx.Document(fileDir+'/'+file)
 
-            # abrir conecção com o arquivo Word 
-            doc = docx.Document(fileDir+'/'+files_array[indice])
+                # data criação
+                dt_doc = doc.core_properties.created
+                # ler em cada parágrafo dentro do arquivo Word
+                paragra= [p.text for p in doc.paragraphs]
 
-            # data criação
-            dt_doc = doc.core_properties.created
-            # ler em cada parágrafo dentro do arquivo Word
-            paragra= [p.text for p in doc.paragraphs]
+                # extai a quantidade de caracteres
+                caracteres = docx2txt.process(fileDir+'/'+file)
 
-            # extai a quantidade de caracteres
-            caracteres = docx2txt.process(fileDir+'/'+files_array[indice])
+                # gerar um array com tamanho de todas as imagens
+                for image in doc.inline_shapes:
+                    image_array.append([image.width, image.height])
+                    # print (image.width, image.height)
 
-            # gerar um array com tamanho de todas as imagens
-            for image in doc.inline_shapes:
-                image_array.append([image.width, image.height])
-                # print (image.width, image.height)
-
-            # gerar um array com todas as tabelas
-            for table in doc.tables:
-                table_chunks.append(table)
-            return(files_array[indice][2:8],len(caracteres), len(table_chunks), len(image_array),dt_doc.date())
+                # gerar um array com todas as tabelas
+                for table in doc.tables:
+                    table_chunks.append(table)
+                    
+                # gerar lista resposta
+                list_retur=file[2:8],len(caracteres), len(table_chunks), len(image_array),dt_doc.date()
+                
+                # gerar tabela resposta
+                table_return.append(list_retur)
+            #return(files_array[indice][2:8],len(caracteres), len(table_chunks), len(image_array),dt_doc.date())
+            return table_return
             #break
         except ValueError:
             print("Oops!  erro no método verificar diret'ório e extensão do arquivos no argumento desse método")
